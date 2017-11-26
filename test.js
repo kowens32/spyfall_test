f42c64681c3f9d71cc73afffc388dc19d0f77362
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-11-26 14:44:15 -0500

    fixed the new game

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
   $(document).on("click", ".new", function renderNewGame () {
       var Name = $('<input type="form" value="Name"/>');
       var enterGame = $('<input type="button" value="enterGame"/>')
-      $("body").append(Name);
-      $("body").append(enterGame);
+      $( this ).replaceWith(Name, enterGame);
+      $('.join').remove();
 
   })
 });
