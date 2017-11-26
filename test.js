19366766e2de5fb62ae81e9748b2ead6e754cc23
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-11-26 14:47:15 -0500

    updated all the buttons

--- test.js
+++ test.js
@@ -1,11 +1,21 @@
 $(document).ready(function () {
 
   $(document).on("click", ".new", function renderNewGame () {
-      var Name = $('<input type="form" value="Name"/>');
+      var name = $('<input type="form" value="name"/>');
       var enterGame = $('<input type="button" value="enterGame"/>')
-      $( this ).replaceWith(Name, enterGame);
+      var back = $('<input type="button" value="back"/>')
+      $( this ).replaceWith(name, enterGame, back);
       $('.join').remove();
 
   })
+    $(document).on("click", ".join", function renderJoinGame () {
+        var name = $('<input type="form" value="name"/>');
+        var code = $('<input type="form" value="code"/>');
+        var joinGame = $('<input type="button" value="joinGame"/>')
+        var back = $('<input type="button" value="back"/>')
+        $( this ).replaceWith(name, code, joinGame, back);
+        $('.new').remove();
+
+    })
 });
 
